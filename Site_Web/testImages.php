da4075bcbe27bfee4a9408ee5a2af7ca50d90e7e
eliott.jaquier@cpnv.ch <eliott.jaquier@cpnv.ch>
2021-03-28 22:26:07 +0200

    Script doc updated

--- Site_Web/testImages.php
+++ Site_Web/testImages.php
@@ -2,7 +2,7 @@
 /**
  * Project : ProgWEB - Images upload site
  * @file      testImages.php
- * @brief     This --- is used to ---
+ * @brief     This test is used to test the image model
  * @author    Created by Eliott.JAQUIER
  * @version   1.0 (22.03.2021)
  */
