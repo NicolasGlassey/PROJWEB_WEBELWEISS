036f3d810236e241b4062482903322f906304434
EliottJaquierCPNV <61775618+EliottJaquierCPNV@users.noreply.github.com>
2021-06-16 10:11:13 +0200

    test for form upload

---
+++ Site_Web/testFormUpload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Project : ProgWEB - Images upload site
+ * @file      testFormUpload.php
+ * @brief     This --- is used to ---
+ * @author    Created by eliot
+ * @version   1.0 (16.06.2021)
+ */
+print_r($_POST);
+print_r($_GET);
+print_r($_FILES);
+?>
+
